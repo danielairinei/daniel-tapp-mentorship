52f130b873a67a9e1c5ce0820b8ef4c1af87e103
danielairinei <airinei.daniel27@gmail.com>
2022-07-28 16:45:16 +0300

    ´

--- app/src/main/java/com/example/mentorship/features/fragmentsdemo/fragments/PasswordFragment.kt
+++ app/src/main/java/com/example/mentorship/features/fragmentsdemo/fragments/PasswordFragment.kt
@@ -18,6 +18,11 @@ class PasswordFragment : Fragment(R.layout.fragment_password) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.passwordTV.text = arguments?.getString("password")
+
+        binding.submitNewPasswordBtn.setOnClickListener {
+            val newPassword = binding.changePasswordEt.text.toString()
+            changePassword(newPassword)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,11 +31,18 @@ class PasswordFragment : Fragment(R.layout.fragment_password) {
             override fun handleOnBackPressed() {
                 if (onBackPressed) {
                     parentFragmentManager.popBackStack()
+
                 }
                 onBackPressed = true
                 Toast.makeText(activity, R.string.fragment_back_navigation_toast_description, Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    private fun changePassword(newPassword: String) {
+        binding.changePasswordEt.setText("")
+        binding.passwordTV.text = newPassword
+        arguments?.putString("password", newPassword)
     }
 
 }
